90fdadc597a48d6d417a7b1581b162e34c590ef6
Ryan Green <rgreen@csumb.edu>
2016-02-25 00:43:20 -0800

    Added files via upload
    Just a shell of a .js file. I'll have a lot more relatively soon.

---
+++ app.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var speed;
+var timestamp;
+var latitude_s;
+var longitude_s;
+var elevation_s;
+var latitude_e;
+var longitude_e;
+var elevation;
+var sender;
+var battery_life;
+var leftOpen = false;
+var rightOpen = false;
+var frontOn = false;
+var backOn = false;
+var picture;
+var packageID;
+
+function UpdateValues()
+{
+
+}
+
