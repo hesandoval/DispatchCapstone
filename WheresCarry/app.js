6bbd47199ec9004a604ad0e399d844f63def1426
Hector Sandoval <hesandova@gmail.com>
2016-03-04 13:58:20 -0800

    Fixed io bug

--- WheresCarry/app.js
+++ WheresCarry/app.js
@@ -1,37 +1,39 @@
 var express = require('express');
 var path = require('path');
 var http = require('http');
-var socketio =require('socket.io');
 var app = express();
 var r = require('rethinkdb');
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 
+
+var server = http.Server(app);
+//var server = http.createServer(app);
+var io = require('socket.io')(server);
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-var server = http.createServer(app)
-
-var io = socketio(server);
+//io.on('connection', function(socket){
+io.on('connection', function(socket) {
+    console.log("User connected to the page");
+    console.log("Socket: " +socket);
+    //var connection = null;
+    //r.connect({host: 'localhost', port: 28015}, function (err, conn) {
+    //    if (err) throw err;
+    //    connection = conn;
+    //    r.table('test').filter(r.row('sender').eq('Carry1')).run(connection, function(err, cursor){
+    //        if (err) throw err;
+    //        cursor.toArray(function(err, result){
+    //          if (err) throw err;
+    //            console.log(JSON.stringify(result, null, 2));
+    //        });
+    //    });
+    //});
+});
 
 server.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
-});
-
-io.on("connection", function(socket) {
-    console.log("User connected to the page");
-    var connection = null;
-    r.connect({host: 'localhost', port: 28015}, function (err, conn) {
-        if (err) throw err;
-        connection = conn;
-        r.table('test').filter(r.row('sender').eq('Carry1')).run(connection, function(err, cursor){
-            if (err) throw err;
-            cursor.toArray(function(err, result){
-              if (err) throw err;
-                console.log(JSON.stringify(result, null, 2));
-            });
-        });
-    });
 });
