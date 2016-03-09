ff15dad1a0e6453a277ac7030a839d4d72bb9147
Hector Sandoval <hesandova@gmail.com>
2016-03-09 13:45:35 -0800

    Commented photo debugging code

--- Kafka/py/Consumer/TestConsumer.py
+++ Kafka/py/Consumer/TestConsumer.py
@@ -14,21 +14,29 @@ myConsumer = KafkaConsumer('test_topic', value_deserializer=msgpack.unpackb,
                             bootstrap_servers=['localhost:9092'])
 
 for message in myConsumer:
-    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                      message.offset, message.key,
-                                      message.value))
+    # print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+    #                                   message.offset, message.key,
+    #                                   message.value))
     data = message.value
-    if len(data['carry_data_current']['photograph']) != 0:
-        photographData = data['carry_data_current']['photograph'][0]
-        outfile = "%s_%d_%d.jpg" % (message.topic, message.partition,message.offset)
-        # pic = cStringIO.StringIO()
-        # # image_string = cStringIO.StringIO(base64.b64decode(photographData))
-        # # image = Image.open(image_string)
-        # # image.save(pic, image.format, quality=100)
-        # # pic.seek(0)
-        with open(DIRECTORY+outfile, "wb") as fh:
-            fh.write(photographData)
-        # fh.close()
+
+
+    #TODO Add rethinkDB code here
+    '''
+    data contains the dictionary object that contains data
+    '''
+
+
+    # if len(data['carry_data_current']['photograph']) != 0:
+    #     photographData = data['carry_data_current']['photograph'][0]
+    #     outfile = "%s_%d_%d.jpg" % (message.topic, message.partition,message.offset)
+    #     # pic = cStringIO.StringIO()
+    #     # # image_string = cStringIO.StringIO(base64.b64decode(photographData))
+    #     # # image = Image.open(image_string)
+    #     # # image.save(pic, image.format, quality=100)
+    #     # # pic.seek(0)
+    #     with open(DIRECTORY+outfile, "wb") as fh:
+    #         fh.write(photographData)
+    #     # fh.close()
 
 
 myConsumer.close()
