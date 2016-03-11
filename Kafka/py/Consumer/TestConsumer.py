cd776cb5f189b5457acd6c6b9be564539c0d99d1
Hector Sandoval <hesandova@gmail.com>
2016-03-11 13:47:24 -0800

    Added more env setup variables fixed some syntx errors

--- Kafka/py/Consumer/TestConsumer.py
+++ Kafka/py/Consumer/TestConsumer.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from kafka import KafkaConsumer
 import rethinkdb as r
+import argparse
 from rethinkdb.errors import RqlRuntimeError, RqlDriverError
 import msgpack
 import os
@@ -9,60 +10,66 @@ __author__ = 'Edgar Sandoval'
 
 
 #Environment variables
+DIRECTORY = 'SupportFiles/KafkaTransfers/'
 RDB_HOST = os.environ.get('RDB_HOST') or 'localhost'
 RDB_PORT = os.environ.get('RDB_PORT') or 28015
 DISPATCH_DB = 'dispatch'
 TABLE = 'test'
 
 #TODO Add database configuration here
-def dbSetup(connection):
+def dbSetup():
     #code here
     connection = r.connect(host=RDB_HOST, port=RDB_PORT)
-    dispatch = r.db(DISPATCH_DB).table(TABLE)
-
     try:
-        r.db_create(TODO_DB).run(connection)
-        r.db(TODO_DB).table_create('dispatch').run(connection)
-        print 'Database completed. Run application'
+        r.db_create(DISPATCH_DB).run(connection)
+        r.db(DISPATCH_DB).table_create('dispatch').run(connection)
+        print('Database completed. Run application')
     except RqlRuntimeError:
-        print 'Database already exists. Run Application' 
+        print('Database already exists. Run Application')
     finally:
         connection.close()
 
 def dbGetConnection():
-    connection = r.connect(host=RDB_HOST, port=RDB_PORT, db=TODO_DB)
+    connection = r.connect(host=RDB_HOST, port=RDB_PORT)
     table = r.db(DISPATCH_DB).table(TABLE)
     return (connection, table)
 
 if __name__ == "__main__":
-    DIRECTORY = 'SupportFiles/KafkaTransfers/'
+
+    parser = argparse.ArgumentParser(description='Run the Flask todo app')
+    parser.add_argument('--setup', dest='run_setup', action='store_true')
+
+    args = parser.parse_args()
+    if args.run_setup:
+        dbSetup()
+    else:
+
+        if not os.path.exists(DIRECTORY):
+            os.makedirs(DIRECTORY)
+
+        myConsumer = KafkaConsumer('test_topic', value_deserializer=msgpack.unpackb,
+                                    bootstrap_servers=['localhost:9092'])
+
+        for message in myConsumer:
+            # print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+            #                                   message.offset, message.key,
+            #                                   message.value))
+            data = message.value
 
 
-    if not os.path.exists(DIRECTORY):
-        os.makedirs(DIRECTORY)
-
-    myConsumer = KafkaConsumer('test_topic', value_deserializer=msgpack.unpackb,
-                                bootstrap_servers=['localhost:9092'])
-
-    for message in myConsumer:
-        # print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-        #                                   message.offset, message.key,
-        #                                   message.value))
-        data = message.value
+            #TODO Add rethinkDB code here
+            # This will now insert the data into the rethinkdb
+            connection, table = dbGetConnection()
+            result = table.insert(data).run(connection)
 
 
-        #TODO Add rethinkDB code here
-        # This will now insert the data into the rethinkdb
-        connection, table = dbGetConnection()
-        table.insert(data).run(connection)
+            if len(data['carry_data_current']['photograph']) != 0:
+                 photographData = data['carry_data_current']['photograph'][0]
+                 outfile = "%s_%d_%d.jpg" % (message.topic, message.partition,message.offset)
+                 with open(DIRECTORY+outfile, "wb") as fh:
+                     fh.write(photographData)
 
+            #TODO check result for valid parameters
 
-        if len(data['carry_data_current']['photograph']) != 0:
-             photographData = data['carry_data_current']['photograph'][0]
-             outfile = "%s_%d_%d.jpg" % (message.topic, message.partition,message.offset)
-             with open(DIRECTORY+outfile, "wb") as fh:
-                 fh.write(photographData)
-
-
-    myConsumer.close()
+        myConsumer.close()
 
