2bf9d98b61856aca7c22cbacddaa563a9d908d8e
Nick Ishihara <nicholes86@gmail.com>
2016-03-11 13:25:33 -0800

    Adding the database setup
    Database setup and connection

--- Kafka/py/Consumer/TestConsumer.py
+++ Kafka/py/Consumer/TestConsumer.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from kafka import KafkaConsumer
 import rethinkdb as r
+from rethinkdb.errors import RqlRuntimeError, RqlDriverError
 import msgpack
 import os
 
@@ -10,12 +11,21 @@ __author__ = 'Edgar Sandoval'
 #TODO Add database configuration here
 def dbSetup(connection):
     #code here
-    connection = r.connect("localhost", 28015)
-    dispatch = r.db('dispatch').table('test')
+    connection = r.connect(host=RDB_HOST, port=RDB_PORT)
+    dispatch = r.db(DISPATCH_DB).table(TABLE)
+
+    try:
+        r.db_create(TODO_DB).run(connection)
+        r.db(TODO_DB).table_create('dispatch').run(connection)
+        print 'Database completed. Run application'
+    except RqlRuntimeError:
+        print 'Database already exists. Run Application' 
+    finally:
+        connection.close()
 
 def dbGetConnection():
-    connection = r.connect("localhost", 28015)
-    table = r.db('dispatch').table('test')
+    connection = r.connect(host=RDB_HOST, port=RDB_PORT, db=TODO_DB)
+    table = r.db(DISPATCH_DB).table(TABLE)
     return (connection, table)
 
 if __name__ == "__main__":
