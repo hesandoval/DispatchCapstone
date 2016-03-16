eedd4f2c2405aa5ccbfb2fc15cc9e22e74bc54e1
Ryan Green <rgreen@csumb.edu>
2016-03-16 13:09:19 -0700

    bugs

--- WheresCarry/app.js
+++ WheresCarry/app.js
@@ -55,8 +55,7 @@ io.on("connection", function(socket) {
 r.connect({host:'localhost', port:28015}, function(err, conn){
   if(err) throw err;
   connection = conn;
-  }
-)};
+  });
 
 r.table('test').changes().run(connection, function(err, cursor){
   if(err) throw err;
