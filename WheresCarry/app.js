78fa72495389bf5fbc0dd72babe0473a56d3c328
Ryan Green <rgreen@csumb.edu>
2016-03-15 22:10:52 -0700

    server functionality
    now listens for changes in the server to post. commented code is for
    once we have a webpage with a form to submit looking for a specific trip
    id

--- WheresCarry/app.js
+++ WheresCarry/app.js
@@ -1,16 +1,33 @@
 var express = require('express');
 var path = require('path');
 var http = require('http');
+var logger = require('morgan');
 var socketio =require('socket.io');
-var app = express();
+var hogan = require('hogan-express');
 var r = require('rethinkdb');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var connection = null;
+
+
+var routes = require('./routes/index');
+
+var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
+app.engine('html', hogan);
+app.set('view engine', 'html');
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
+
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
 
 var server = http.createServer(app)
 
@@ -22,7 +39,6 @@ server.listen(app.get('port'), function () {
 
 io.on("connection", function(socket) {
     console.log("User connected to the page");
-    var connection = null;
     r.connect({host: 'localhost', port: 28015}, function (err, conn) {
         if (err) throw err;
         connection = conn;
@@ -35,3 +51,28 @@ io.on("connection", function(socket) {
         });
     });
 });
+
+r.connect({host:'localhost', port:28015}, function(err, conn){
+  if(err) throw err;
+  connection = conn;
+  }
+)};
+
+r.table('test').changes().run(connection, function(err, cursor){
+  if(err) throw err;
+  cursor.each(function(err,row){
+    if(err) throw err;
+console.log(JSON.stringify(row,null,2));
+});
+});
+
+/*
+var tid = document.getElementById("TripNumber).value;
+
+r.table('test').filter(r.row('trip_id').eq(tid)).run(connection, function(err, cursor){
+  if(err) throw err;
+  cursor.toArray(function(err,result){
+    if(err) throw err;
+  });
+});
+*/
