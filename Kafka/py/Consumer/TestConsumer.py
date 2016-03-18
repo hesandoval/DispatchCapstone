67577193458434ab74b9c5bd8dabb51de6b7c39c
Hector Sandoval <hesandova@gmail.com>
2016-03-18 11:42:02 -0700

    Changed comments

--- Kafka/py/Consumer/TestConsumer.py
+++ Kafka/py/Consumer/TestConsumer.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
 
             data = message.value
             if args.with_db:
-            #run with no database
+            #run with database
                 data['carry_data_current']['photograph'] = [r.binary(d) for d in data['carry_data_current']['photograph']]
                 # This will now insert the data into the rethinkdb
                 connection, table = dbGetConnection()
@@ -62,6 +62,7 @@ if __name__ == "__main__":
                 #TODO check result for valid parameters
 
             else:
+                #run with no database
                 if len(data['carry_data_current']['photograph']) != 0:
                     photographData = data['carry_data_current']['photograph'][0]
                     outfile = "%s_%d_%d.jpg" % (message.topic, message.partition,message.offset)
