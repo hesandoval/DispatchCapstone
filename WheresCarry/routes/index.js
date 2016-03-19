21b1efe2cf8a5906b1aa9c7f952a9ddf9b0117a8
Hector Sandoval <hesandova@gmail.com>
2016-03-18 18:02:18 -0700

    Fixed merge conflicts in app.js and added instructions to WheresCarry README file

--- WheresCarry/routes/index.js
+++ WheresCarry/routes/index.js
@@ -1,17 +1,10 @@
 /**
  * Created by edsan on 3/2/16.
  */
-<<<<<<< HEAD
-var express = require('express');
-var router = express.Router();
-
-
-=======
 
 var express = require('express');
 var router = express.Router();
 
->>>>>>> master
 router.get('/', function(req, res){
     res.render('index.html');
 });
