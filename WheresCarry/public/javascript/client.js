8b688f280de3f1e41cc4af0629c0eaf5ba1bc659
Hector Sandoval <hesandova@gmail.com>
2016-03-21 21:07:44 -0700

    Wired one web socket emitter with database

--- WheresCarry/public/javascript/client.js
+++ WheresCarry/public/javascript/client.js
@@ -1,5 +1,14 @@
 var socket = io();
 
+socket.emit('carry:getFleet',function(err,data){
+    console.log(data);
+
+    //$.each(data, function(){
+    //    console.log(this.carry_data_current.sender)
+    //});
+});
+
+
 //socket.on('update-msg', function(msg){
 //    console.log(msg);
 //    $('#div').html(msg.data);
