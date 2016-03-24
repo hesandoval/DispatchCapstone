1ff40357b61b7f796b42f18e7f5dd1ca11bcc39c
Hector Sandoval <hesandova@gmail.com>
2016-03-23 18:24:36 -0700

    Changes

--- WheresCarry/public/javascript/client.js
+++ WheresCarry/public/javascript/client.js
@@ -1,8 +1,13 @@
 var socket = io();
 
 socket.emit('carry:getFleet',function(err,data){
-    console.log(data);
-
+    data = {'stuff' : data};
+    console.log(JSON.stringify(data));
+    var template = '<select>{{ #stuff }}<option>{{ carry_data_current.sender }}</option>{{ /stuff }}</select>';
+    console.log(template);
+    var html = Mustache.to_html(template, data);
+    console.log(html);
+    $('#dropdown_fleet_select').html(html);
     //$.each(data, function(){
     //    console.log(this.carry_data_current.sender)
     //});
