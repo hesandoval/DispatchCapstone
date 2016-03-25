66733a3d385f9399ec09eef89290dcb5ca00a5d9
Hector Sandoval <hesandova@gmail.com>
2016-03-24 21:40:23 -0700

    added functionality to dynamically lookup fleet trip id's

--- WheresCarry/CarryData.js
+++ WheresCarry/CarryData.js
@@ -33,6 +33,20 @@ function setup(io){
                 .pluck({carry_data_current: ["trip_id", "completed"]})
                 .distinct().run(callback);
         });
+        socket.on("carry:tripDetailsByTripID", function(tripID, callback){
+            r.db("dispatch").table('wheres_carry')
+                .filter({carry_data_current:{"trip_id":tripID}})
+                .orderBy(r.desc('created')).run(function(err, data){
+                if(err){
+                    console.log(err);
+                }else{
+                    var first = data[0];
+                    var last = data[data.length-1];
+                    var newData = [first, last];
+                    callback(newData)
+                }
+            });
+        });
         socket.on('carry:chages:start', function(data){
             var filter = data.filter || {};
             r.table('wheres_carry').orderBy({index: r.desc('created')})
--- WheresCarry/public/javascript/client.js
+++ WheresCarry/public/javascript/client.js
@@ -1,13 +1,14 @@
 var socket = io();
 
 socket.emit('carry:getFleet',function(err,data){
-    data = {'stuff' : data};
-    var template = '{{ #stuff }}<option value="{{ carry_data_current.sender }}">{{ carry_data_current.sender }}</option>{{ /stuff }}';
-    var html = Mustache.to_html(template, data);
-    $('#fleet_select').append(html);
-    //$.each(data, function(){
-    //    console.log(this.carry_data_current.sender)
-    //});
+    if(err){
+        console.log(err);
+    }else{
+        data = {'stuff' : data};
+        var template = '{{ #stuff }}<option value="{{ carry_data_current.sender }}">{{ carry_data_current.sender }}</option>{{ /stuff }}';
+        var html = Mustache.to_html(template, data);
+        $('#fleet_select').append(html);
+    }
 });
 
 //
@@ -15,14 +16,42 @@ socket.emit('carry:getFleet',function(err,data){
 //    console.log("Hello");
 //});
 
-function choiceChanged(){
+function fleetchoiceChanged(){
     console.log(document.getElementById('fleet_select').value);
     var fleet = document.getElementById('fleet_select').value;
     socket.emit('carry:findTripsByCarryID', fleet,function(err, data){
         if(err){
             console.log(JSON.stringify(err));
+        }else{
+            var values = {};
+            for (var index in data){
+                if(data[index]["carry_data_current"]["trip_id"] in values){
+                    values[data[index]["carry_data_current"]["trip_id"]] = true;
+                }else {
+                    values[data[index]["carry_data_current"]["trip_id"]] = false;
+                }
+            }
+            var view = {values:[]};
+            for (var index in values){
+                view['values'].push({trip_id:index, finished:values[index]})
+            }
+            console.log(view);
+
+            var template = "<select id='trip_select' onchange='tripSelectChanged()'> <option>Select One</option>" +
+                "{{ #values}}<option value='{{trip_id}}'>{{trip_id}}</option>{{ /values }} " +
+                "</select>";
+            var html = Mustache.to_html(template, view);
+            console.log(html);
+            $("#dropdown_fleet_select").append(html)
         }
-        console.log(JSON.stringify(data))
+
+    });
+}
+function tripSelectChanged() {
+    console.log(document.getElementById('trip_select').value);
+    var tripID = document.getElementById('trip_select').value;
+    socket.emit('carry:tripDetailsByTripID', tripID, function (data) {
+        console.log(JSON.stringify(data));
     });
 }
 
