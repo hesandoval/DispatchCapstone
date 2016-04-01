567aa4af9d8ba492f8acf9b07f064c42992d46c9
Ryan <rgreen@csumb.edu>
2016-04-01 12:59:40 -0700

    historicalData

--- WheresCarry/CarryData.js
+++ WheresCarry/CarryData.js
@@ -56,17 +56,28 @@ function setup(io){
         //     }
         //     else {
                 socket.on("carry:tripDetailsByTripID", function (tripID, callback) {
-                    r.db("dispatch").table('wheres_carry')
-                        .filter({carry_data_current: {"trip_id": tripID}})
-                        .orderBy(r.desc('created')).run(function (err, data) {
+                    r.table('wheres_carry')('carry_data_current')
+                        .filter({"trip_id": tripID})
+                        .orderBy('created').run(function (err, data) {
                         if (err) {
                             console.log(err);
                         } else {
                             var first = data[0];
+                            //console.log(JSON.stringify(first));
                             var last = data[data.length - 1];
+                            //console.log(JSON.stringify(last));
                             var newData = [first, last];
-                            console.log("first test: " + first["carry_data_current"]["current_location"]["longitude"]);
-                            callback(newData);
+                            var g2j = {};
+                            g2j["starting_location"] = {"lat" : first['current_location']['latitude'], "lng": first['current_location']['longitude']};
+                            g2j["ending_location"] = {"lat" : last['current_location']['latitude'], "lng": last['current_location']['longitude']};
+                            g2j["sender"] = {"sender": first['sender']};
+                            g2j["waypoints"] = first['waypoints'];
+                            console.log("first battery " + JSON.stringify(first['battery_life']));
+                            console.log("last battery " + JSON.stringify(last['battery_life']));
+                            var batt  = first['battery_life']-last['battery_life'];
+                            g2j["battery_consumption"] = {"battery_consumption": batt + "%"};
+                            console.log("setting g2j" + JSON.stringify(g2j));
+                            callback(g2j);
                         }
                     });
                 });
